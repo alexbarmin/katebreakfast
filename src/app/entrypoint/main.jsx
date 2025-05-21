4740b7874bee98fde13bfca11a64fd4b05da40f8
Aleksandr Mospan <alexander.mospan@gmail.com>
2025-05-21 14:54:40 +0300

    rm dark theme

--- src/app/entrypoint/main.jsx
+++ src/app/entrypoint/main.jsx
@@ -5,7 +5,7 @@ import '../styles/index.css'
 import App from './App.jsx'
 
 import { Provider } from 'react-redux'
-import store from "../store"
+import { store } from "../store/store"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
